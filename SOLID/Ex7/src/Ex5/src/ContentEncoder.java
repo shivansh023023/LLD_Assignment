d466a41aaae1e3bae423c9b7aa03628317185674
shivansh023023 <168457476+shivansh023023@users.noreply.github.com>
2026-03-01 20:49:42 +0530

    Delete SOLID/Ex7/src/Ex5 directory

--- SOLID/Ex7/src/Ex5/src/ContentEncoder.java
+++
@@ -1,3 +0,0 @@
-public interface ContentEncoder {
-    byte[] encode(ExportRequest req);
-}
--- SOLID/Ex7/src/Ex5/src/PdfEncoder.java
+++
@@ -1,9 +0,0 @@
-import java.nio.charset.StandardCharsets;
-
-public class PdfEncoder implements ContentEncoder {
-    @Override
-    public byte[] encode(ExportRequest req) {
-        String fakePdf = "PDF(" + req.title + "):" + (req.body == null ? "" : req.body);
-        return fakePdf.getBytes(StandardCharsets.UTF_8);
-    }
-}
